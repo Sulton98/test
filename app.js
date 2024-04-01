0c2039ebe06d3a95ddde1e6a59cf07a48399ca57
Sulton98 <bekchanovsultonboy@gmail.com>
2024-04-01 21:18:52 +0500

    ini1

---
+++ app.js
@@ -0,0 +1,3 @@
+// CALLBACK - bu funksiyaning parametrida funksiya kelishi;
+// HIGH ORDER FUNCTIONS - fuksiya parametrida funksiya kelsa va return ida funksiya qaytarsa HIGH ORDER FUNCTION bo`ladi;
+console.log ("INIT")
